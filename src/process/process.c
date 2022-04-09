dd1c5b3568fac2721471c16170d58cf28d480e23
diaztunjano <dfdiaz1@uc.cl>
2022-04-09 19:49:42 -0400

    feat(process-struct): add struct init in .c file

--- src/process/process.c
+++ src/process/process.c
@@ -1,5 +1,13 @@
 #include "process.h"
 
-Process *processInit(char NOMBRE_PROCESO, int PID, int TIEMPO_INICIO, int CYCLES, int WAIT, int WAITING_DELAY, int S){
-    
+Process *processInit(char NOMBRE_PROCESO, int PID, int TIEMPO_INICIO, int CYCLES, int WAIT, int WAITING_DELAY, int S)
+{
+    Process *new_process = calloc(1, sizeof(Process));
+    new_process->pid = PID;
+    new_process->start_time = TIEMPO_INICIO;
+    new_process->cycles = CYCLES;
+    new_process->wait = WAIT;
+    new_process->waiting_delay = WAITING_DELAY;
+    new_process->S_aging_time = S;
+    return new_process
 }
