1939b80b8df2a6a59195de6ee14f4e043b7fd3b2
diaztunjano <dfdiaz1@uc.cl>
2022-04-09 19:45:36 -0400

    feat(process-struct): Add more parameters info to struct

--- src/process/process.c
+++ src/process/process.c
@@ -1,1 +1,5 @@
 #include "process.h"
+
+Process *processInit(char NOMBRE_PROCESO, int PID, int TIEMPO_INICIO, int CYCLES, int WAIT, int WAITING_DELAY, int S){
+    
+}
--- src/process/process.h
+++ src/process/process.h
@@ -9,7 +9,7 @@ typedef struct process Process;
 
 struct process
 {
-    /** El valor que guarda el pixel */
+    /** Enunciado info minima */
     int pid;
     char *name[32];
     int priority;
@@ -21,10 +21,36 @@ struct process
         FINISHED
     };
 
-    // Times
-    int remaining_time;
-    int time_a_cpu_burst;
-    int time_b_io_burst;
+    int start_time;
+    int cycles;
+    int wait;
+    int waiting_delay;
+    int S_aging_time;
+
+    // ENUNCIADO -- output
+
+    // El numero de veces que el proceso fue elegido para usar alguna CPU
+    int cpu_number_choice;
+    // El numero de veces que fue interrumpido. Este equivale al n ´ umero de veces que el ´ scheduler saco al proceso de la CPU
+    int cpu_number_interruptions;
+    // Turnaround time
+    int turnaround_time;
+    // Response time
+    int response_time;
+    // Waiting time (suma del tiempo en el que el proceso esta en estado READY y WAITING.)
+    int waiting_time;
+    // Numero de ciclos en CPU
+    int cpu_exec_counter;
+    // Ciclos que le faltan para terminar
+    int cycles_left_counter;
+    // Ciclos de envejecimiento
+    int s_aging_counter;
+    // Ciclos del wait actual
+    int wait_counter; 
+
+    // Order de procesos
+    Process* next;
+    Process* prev;
 };
 
-Process *processInit();
+Process *processInit(char NOMBRE_PROCESO, int PID, int TIEMPO_INICIO, int CYCLES, int WAIT, int WAITING_DELAY, int S);
