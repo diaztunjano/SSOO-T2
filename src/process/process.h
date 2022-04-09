3622bf8414441845df6c33a05638007987ab693c
diaztunjano <dfdiaz1@uc.cl>
2022-04-09 19:27:38 -0400

    feat(struct process): initial creation

--- src/process/process.h
+++ src/process/process.h
@@ -1,1 +1,30 @@
+// Incluimos standard I/O, standard library
+#include <stdio.h>
+#include <stdlib.h>
+
 #pragma once
+
+struct process;
+typedef struct process Process;
+
+struct process
+{
+    /** El valor que guarda el pixel */
+    int pid;
+    char *name[32];
+    int priority;
+    enum status
+    {
+        RUNNING,
+        READY,
+        WAITING,
+        FINISHED
+    };
+
+    // Times
+    int remaining_time;
+    int time_a_cpu_burst;
+    int time_b_io_burst;
+};
+
+Process *processInit();
--- src/queue/queue.h
+++ src/queue/queue.h
@@ -1,1 +1,28 @@
+// Incluimos standard I/O, standard library
+#include <stdio.h>
+#include <stdlib.h>
+
 #pragma once
+
+struct queue;
+typedef struct queue Queue;
+
+struct queue
+{
+    /** El valor que guarda el pixel */
+    int pid;
+    char name[32];
+    int priority;
+    enum status
+    {
+        RUNNING,
+        READY,
+        WAITING,
+        FINISHED
+    };
+
+    // Times
+    int remaining_time;
+    int time_a_cpu_burst;
+    int time_b_io_burst;
+};
