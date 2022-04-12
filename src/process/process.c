e0f56cf982831cc14bceb3e16e79cb76a05ac689
Manuel <mimunoz11@uc.cl>
2022-04-12 00:42:58 -0400

    Changes in procces + Delete process

--- src/process/process.c
+++ src/process/process.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "process.h"
 
-Process *processInit(char NOMBRE_PROCESO, int PID, int TIEMPO_INICIO, int CYCLES, int WAIT, int WAITING_DELAY, int S)
+Process *processInit(char NOMBRE_PROCESO, int PID, int TIEMPO_INICIO, int CYCLES, int WAIT, int WAITING_DELAY, int S, enum DesignStatus P_STATUS, int PRIORITY)
 {
     Process *new_process = calloc(1, sizeof(Process));
     new_process->pid = PID;
@@ -12,5 +12,13 @@ Process *processInit(char NOMBRE_PROCESO, int PID, int TIEMPO_INICIO, int CYCLES
     new_process->wait = WAIT;
     new_process->waiting_delay = WAITING_DELAY;
     new_process->s_aging_time = S;
+    new_process->p_status = P_STATUS;
+    new_process->priority = PRIORITY;
     return new_process;
+}
+
+
+void delete_process(Process* new_process)
+{
+  free(new_process);
 }
